8d58785bcb4e87173415e191a64191b721e80634
YongBum <dt10002@naver.com>
2025-02-24 23:41:53 +0900

    :tada: Init: 프로젝트 생성 # 본문은 아래에 작성 - 기뿌 프로젝트를 생성합니다. # 꼬릿말은 아래에 작성: ex) Github issue #23 issue #1 # --- COMMIT END --- #   <타입> 리스트 #   feat        : 기능 (새로운 기능) #   fix         : 버그 (버그 수정) #   refactor    : 리팩토링 #   design      : CSS 등 사용자 UI 디자인 변경 #   comment     : 필요한 주석 추가 및 변경 #   style       : 스타일 (코드 형식, 세미콜론 추가: 비즈니스 로직에 변경 없음) #   docs        : 문서 수정 (문서 추가, 수정, 삭제, README) #   test        : 테스트 (테스트 코드 추가, 수정, 삭제: 비즈니스 로직에 변경 없음) #   chore       : 기타 변경사항 (빌드 스크립트 수정, assets, 패키지 매니저 등) #   init        : 초기 생성 #   rename      : 파일 혹은 폴더명을 수정하거나 옮기는 작업만 한 경우 #   remove      : 파일을 삭제하는 작업만 수행한 경우 # ------------------ #   제목 첫 글자를 대문자로 #   제목은 명령문으로 #   제목 끝에 마침표(.) 금지 #   제목과 본문을 한 줄 띄워 분리하기 #   본문은 "어떻게" 보다 "무엇을", "왜"를 설명한다. #   본문에 여러줄의 메시지를 작성할 땐 "-"로 구분 # ------------------ #   <꼬리말> #   필수가 아닌 optioanl #   Fixes        :이슈 수정중 (아직 해결되지 않은 경우) #   Resolves     : 이슈 해결했을 때 사용 #   Ref          : 참고할 이슈가 있을 때 사용 #   Related to   : 해당 커밋에 관련된 이슈번호 (아직 해결되지 않은 경우) #   ex) Fixes: #47 Related to: #32, #21

---
+++ src/main/java/personal/gibbu/GibbuApplication.java
@@ -0,0 +1,13 @@
+package personal.gibbu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GibbuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GibbuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/personal/gibbu/GibbuApplicationTests.java
@@ -0,0 +1,13 @@
+package personal.gibbu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GibbuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
